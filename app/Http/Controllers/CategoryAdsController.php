63c5f1fddd0ee53fed947343c475884c3ae9dfd0
DESKTOP-6BA70TQ\Siswo Handoko <cs.siswo.handoko@gmail.com>
2018-01-15 21:36:25 +0700

    commit model structure

--- app/Http/Controllers/CategoryAdsController.php
+++ app/Http/Controllers/CategoryAdsController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\CategoryAds;
+use App\Model\CategoryAds;
 class CategoryAdsController extends Controller
 {
     /**
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\User;
+use App\Model\User;
 class UserController extends Controller
 {
     /**
