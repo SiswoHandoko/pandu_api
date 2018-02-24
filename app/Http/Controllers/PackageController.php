9e10ad25b38a23d5a92f132676061b89971ebab8
asep mulyadi <asep.mulyadi011@gmail.com>
2018-02-23 23:03:02 -0800

    Update postman and get plan by user

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -55,6 +55,7 @@ class PackageController extends Controller
             'name' => 'required',
             'description' => 'required',
             'days' => 'required',
+            'image_url' => 'required',
             'start_date' => 'required',
             'end_date' => 'required'
         ]);
@@ -69,6 +70,7 @@ class PackageController extends Controller
             $package->name = $req->has('name') ? $req->name : '';
             $package->description = $req->has('description') ? $req->description : '';
             $package->days = $req->has('days') ? $req->days : '';
+            $package->image_url = $value ? $this->uploadFile($this->public_path(). "/images/packages/", $value) : 'default_img.png';
             $package->start_date = $req->has('start_date') ? $req->start_date : '000-00-00';
             $package->end_date = $req->has('end_date') ? $req->end_date : '000-00-00';
             $package->status = $req->has('status') ? $req->status : 'active';
@@ -117,6 +119,7 @@ class PackageController extends Controller
             'name' => 'required',
             'description' => 'required',
             'days' => 'required',
+            'image_url' => 'required',
             'start_date' => 'required',
             'end_date' => 'required'
         ]);
@@ -136,6 +139,7 @@ class PackageController extends Controller
                 $package->name = $req->has('name') ? $req->name : $package->name;
                 $package->description = $req->has('description') ? $req->description : $package->description;
                 $package->days = $req->has('days') ? $req->days : $package->days;
+                $package->image_url = $req->has('image_url') ? $this->uploadFile($this->public_path(). "/images/packages/", $req->image_url, $package->image_url) : $package->image_url;
                 $package->start_date = $req->has('start_date') ? $req->start_date : $package->start_date;
                 $package->end_date = $req->has('end_date') ? $req->end_date : $package->end_date;
                 $package->status = $req->has('status') ? $req->status : $package->status;
